fbaf98ba5315312a72f95de10430ad1139f4193a
shmid85 <martinukag@mail.ru>
2019-03-28 19:25:58 +0400

    Added SkyMap construtor, helpers.js with different functions,

---
+++ GameObjects/SkyMap.js
@@ -0,0 +1,38 @@
+/**
+ * Created by SHMID on 28.03.2019.
+ */
+function SkyMap(columnNum, rowNum) {
+    if(!isInterger(columnNum)) {
+        throw new TypeError("culumnNum isn't Interger");
+    };
+
+    if(!isInterger(rowNum)) {
+        throw new TypeError("rowNum isn't Interger");
+    };
+
+    if(columnNum <= 0){
+        throw new TypeError("culumnNum is less or equal to 0");
+    };
+
+    if(rowNum <= 0){
+        throw new TypeError("rowNum is less or equal to 0");
+    };
+
+    this.columnNum = columnNum;
+    this.rowNum = rowNum;
+};
+
+SkyMap.prototype = {
+    constructor : SkyMap,
+    isCellInMap : function (mapCell) {
+        if(! (mapCell instanceof MapCell)) {
+            throw new TypeError("Argument isn't MapCell object");
+        };
+
+        if(mapCell.x <0 || mapCell.y <0 || mapCell.x >= this.columnNum || mapCell.y >= this.rowNum) {
+            return false;
+        };
+
+        return true;
+    }
+};
---
+++ Helpers.js
@@ -0,0 +1,7 @@
+/**
+ * Created by SHMID on 28.03.2019.
+ */
+
+function isInterger(number) {
+    return (number ^ 0) === number;
+}
